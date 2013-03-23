1ec98f1a5514781bfb7ddf37bcc87a09913d29b6
unknown <diogothesilva@gmail.com>
2013-03-23 16:11:05 +0000

    Esqueleto do projecto criado pelo Nuno

---
+++ DataServer/DataServer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting;
+
+namespace PADI_FS
+{
+    class DataServer
+    {
+        static void Main(string[] args)
+        {
+            TcpChannel channel = new TcpChannel(8086);
+            ChannelServices.RegisterChannel(channel, true);
+            MyRemoteObject mo = new MyRemoteObject();
+            RemotingServices.Marshal(mo,
+                "MyRemoteObjectName",
+                typeof(MyRemoteObject));
+            System.Console.WriteLine("<enter> para sair do Data Server...");
+            System.Console.ReadLine();
+        }
+    }
+
+    public class MyRemoteObject : MarshalByRefObject
+    {
+        public string MetodoOla()
+        {
+            return "ola!";
+        }
+    }
+}
